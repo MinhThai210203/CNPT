95042bf33c3eca95183cb937f23aebd8fb888ed7
MinhThai210203 <taminthai210203@gmail.com>
2025-04-09 11:29:02 +0700

    Add files via upload

---
+++ DOAN CNPT/LNBT/Dto/ProductDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LNBT.Dto
+{
+    public class ProductDTO
+    {
+        public int MaSanPham { get; set; }
+        public string TenSanPham { get; set; }
+        public decimal Gia { get; set; }
+        public string TenDanhMuc { get; set; }
+        public string MoTa { get; set; }
+        public string TrangThai { get; set; }
+        public decimal? KhuyenMai { get; set; }
+    }
+}
