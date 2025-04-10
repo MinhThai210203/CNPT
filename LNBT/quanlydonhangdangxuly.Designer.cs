e57f9187b8a8e206856f81b77b7dcf5051725a19
THANH SANG <THANHSANG2112004@gmail.com>
2025-04-11 01:22:01 +0700

    Thực hiện viết code xử lý

--- LNBT/quanlydonhangdangxuly.Designer.cs
+++ LNBT/quanlydonhangdangxuly.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -41,16 +42,24 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(350, 212);
+            this.textBox2.Location = new System.Drawing.Point(121, 90);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 26);
             this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(350, 212);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 2;
             // 
             // quanlydonhangdangxuly
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "quanlydonhangdangxuly";
@@ -64,5 +73,6 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
