9b4ec89994c82abcb75362ac2db86eeb292b3a2f
THANH SANG <THANHSANG2112004@gmail.com>
2025-04-11 01:12:30 +0700

    Thực hiện liên kết giữa giao diện và xử lý

--- LNBT/tichdiemvasudungmagiamgia.Designer.cs
+++ LNBT/tichdiemvasudungmagiamgia.Designer.cs
@@ -32,6 +32,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -57,16 +58,24 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(350, 212);
+            this.textBox4.Location = new System.Drawing.Point(505, 218);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 26);
             this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(228, 282);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 26);
+            this.textBox5.TabIndex = 4;
             // 
             // tichdiemvasudungmagiamgia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -84,5 +93,6 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
