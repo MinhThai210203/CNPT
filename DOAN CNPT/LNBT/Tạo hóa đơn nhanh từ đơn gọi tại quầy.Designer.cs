c2dee14ce766df0cd5802dbb87e9505eac5cedc2
tzgiabao <baor2711@gmail.com>
2025-04-10 23:07:31 +0700

    feat: chuẩn bị rebase để push

---
+++ DOAN CNPT/LNBT/Tạo hóa đơn nhanh từ đơn gọi tại quầy.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace LNBT
+{
+    partial class Tạo_hóa_đơn_nhanh_từ_đơn_gọi_tại_quầy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(294, 144);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Tạo_hóa_đơn_nhanh_từ_đơn_gọi_tại_quầy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Tạo_hóa_đơn_nhanh_từ_đơn_gọi_tại_quầy";
+            this.Text = "Tạo_hóa_đơn_nhanh_từ_đơn_gọi_tại_quầy";
+            this.Load += new System.EventHandler(this.Tạo_hóa_đơn_nhanh_từ_đơn_gọi_tại_quầy_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
