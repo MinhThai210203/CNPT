cacfaf667a4c672b84b06dc0d074b3a11a922286
MinhThai210203 <taminthai210203@gmail.com>
2025-04-10 22:34:36 +0700

    Thực hiện viết code xử lý

--- LNBT/xemdongiaohang.Designer.cs
+++ LNBT/xemdongiaohang.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -46,11 +47,19 @@
             this.textBox2.Size = new System.Drawing.Size(109, 22);
             this.textBox2.TabIndex = 1;
             // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(190, 239);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(135, 22);
+            this.textBox3.TabIndex = 2;
+            // 
             // xemdongiaohang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "xemdongiaohang";
@@ -64,5 +73,6 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
