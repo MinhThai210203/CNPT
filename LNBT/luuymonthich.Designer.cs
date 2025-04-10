11f12fdb5a9188f515734e805882b145b24d1a32
THANH SANG <THANHSANG2112004@gmail.com>
2025-04-10 22:51:26 +0700

    Code class user gồm các thuộc tính như thiết kế

--- LNBT/luuymonthich.Designer.cs
+++ LNBT/luuymonthich.Designer.cs
@@ -29,29 +29,20 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(623, 98);
+            this.textBox1.Location = new System.Drawing.Point(549, 120);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 26);
             this.textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(546, 144);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 26);
-            this.textBox2.TabIndex = 1;
             // 
             // luuymonthich
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "luuymonthich";
             this.Text = "luuymonthich";
@@ -63,6 +54,5 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
     }
 }
